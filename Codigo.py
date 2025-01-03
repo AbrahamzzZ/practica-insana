c12aafc84fc3ff3cc9c02f9aba84411c8e5fbbe4
AbrahamzzZ <hermanosfarfan@gmail.com>
2025-01-02 20:15:42 -0500

    Se agrego una suma bien insana

--- Codigo.py
+++ Codigo.py
@@ -4,3 +4,9 @@ saludo= "Hola "+nombre+" "+apellido+"."
 print(saludo)
 
 print("Hola "+nombre+" "+apellido+", Como estas?")
+
+a = 8
+b = 10
+resultado = 0
+resultado = a + b
+print("El resultado es: ", resultado)
